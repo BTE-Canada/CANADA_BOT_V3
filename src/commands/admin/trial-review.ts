f18dfd61b866133f4047da1e28deb76b0366d4e5
daniel01 <97580498+daniel000111@users.noreply.github.com>
2023-06-08 12:20:23 -0230

    Update trial-review.ts with embeds

--- src/commands/admin/trial-review.ts
+++ src/commands/admin/trial-review.ts
@@ -1,7 +1,9 @@
-import { ApplicationCommandOptionType, ApplicationCommandType, Channel, CommandInteraction, TextChannel, GuildMember } from 'discord.js';
+import Discord, { ApplicationCommandOptionType, ApplicationCommandType, Channel, CommandInteraction, TextChannel, GuildMember, Message } from 'discord.js';
 import { Command } from '../command';
 import { MyClient } from 'src';
 import { DiscordHelpers } from '../../helpers/discord-helpers';
+
+const { EmbedBuilder } = require('discord.js');
 
 const D = false;
 
@@ -37,8 +39,7 @@ export const TrialReview: Command = {
             if (!DiscordHelpers.hasRole(interaction.member as GuildMember, client.config.opt.roles.staff)) {
                 return await interaction.reply("You don't have permission to review trial builds!");
             }
-            const reviewer = interaction.user.tag;
-
+            const reviewer = interaction.user.toString();
             
             // Get Options
             const options = interaction.options;       
@@ -67,7 +68,6 @@ export const TrialReview: Command = {
             const submittingMember = submissionMsg.author;
             if (!submittingMember) return interaction.reply("Failed! Submitter Invalid!");
 
-
             await interaction.reply("Processing...");
             
             if (submissionMsg.reactions.cache.has("✅")) {
@@ -76,48 +76,87 @@ export const TrialReview: Command = {
 
             if (accept) {
 
-                await interaction.followUp(`
-                    **Reviewed!** You have accepted this trial submission.
-                    ${submittingMember.id} has been ranked to **novice builder**
-                `);
-            
-                await submittingMember.send(`
-                    You have been **accepted** into our build team! Your rank is now **Novice Builder**.\n
-                    You can build anywhere now as long as you follow our builder system, which can be found 
-                    in our discord. Welcome aboard!\n
-                    Reviewer: ${reviewer}
+                const embedreply1 = new EmbedBuilder()
+                .setTitle(`**<a:SPINNYCANADAv2:1102005736655036426> REVIEWED SUCCESSFULLY <a:SPINNYCANADAv2:1102005736655036426>**`)
+                .setDescription(`
+                    You have accepted ${submittingMember.toString()}'s build!\n[Submission Link](${submissionMsg.url})
                     ${feedback != undefined ? `\nFeedback: ${feedback}` : ""}
-                `);
+                `)
+                
+                await interaction.followUp({ embeds: [embedreply1] });
+
+                const embedaccept = new EmbedBuilder()
+                    .setTitle(`<a:SPINNYCANADAv2:1102005736655036426> Your recent trial submission has been accepted! <a:SPINNYCANADAv2:1102005736655036426>`)
+                    .setDescription(`
+                        You have been **accepted** into our build team! Your rank is now **Novice Builder**.\n
+                        You can build anywhere now as long as you follow our builder system, which can be found in our discord. Welcome aboard!\n
+                        Reviewer: ${reviewer}
+                        ${feedback != undefined ? `\nFeedback: ${feedback}` : ""}
+                    `)
+                    .setColor(0x00FF00)
+            
+                await submittingMember.send({ embeds: [embedaccept] }).catch((err) => {
+                    return interaction.reply(`
+                        This builder has DMs turned off or something went wrong while sending the DM.
+                    `)
+                });
 
                 await submissionMsg.react("✅");
 
+                const reviewerchannelaccept = new EmbedBuilder()
+                    .setDescription(`
+                    ${submittingMember.toString()}'s trial build was accepted by ${reviewer}.\n[Submission Link](${submissionMsg.url}) 
+                    ${feedback != undefined ? `\nFeedback: ${feedback}` : ""}
+                    `)
+                    .setColor(0x00FF00)
+
                 DiscordHelpers.getTextChannel(client, client.config.opt.channels.hi_reviewers)
                     .then((channel: TextChannel | null) => {
                         if (channel && channel != null) {
-                            channel.send(`${submittingMember.tag}'s trial build was accepted by ${reviewer}`);
+                            channel.send({ embeds: [reviewerchannelaccept] });
                         }
-                    });
+                    });           
 
             } else {
 
-                await interaction.followUp(`
-                    **Reviewed!** You have denied this trial submission.
-                    ${submittingMember.id} has been ranked to **novice builder**
-                `);
-
-                await submittingMember.send(`
-                    Your trial build has been **denied**. But don't worry, this happens to most people! 
-                    Use the feedback given to improve your build, then re-submit again.\n
-                    Reviewer: ${reviewer}
+                const embedreply2 = new EmbedBuilder()
+                .setTitle(`**<a:SPINNYCANADAv2:1102005736655036426> REVIEWED SUCCESSFULLY <a:SPINNYCANADAv2:1102005736655036426>**`)
+                .setDescription(`
+                    You have denied ${submittingMember.toString()}'s build.\n[Submission Link](${submissionMsg.url})
                     ${feedback != undefined ? `\nFeedback: ${feedback}` : ""}
-                `);
+                `)
+                
+                await interaction.followUp({ embeds: [embedreply2] });
+
+                const embeddeny = new EmbedBuilder()
+                    .setTitle(`<a:SPINNYCANADAv2:1102005736655036426> Your recent trial submission has been denied. <a:SPINNYCANADAv2:1102005736655036426>`)
+                    .setDescription(`
+                        Your build has been **declined** from our build team. But don't worry, this happens to most people!.\n
+                        As long as you follow the feedback from the reviewer, your build should be accepted. Please resubmit when you've completed your build.\n
+                        Reviewer: ${reviewer}
+                        ${feedback != undefined ? `\nFeedback: ${feedback}` : ""}
+                    `)
+                    .setColor(0xFF0000)
+
+                await submittingMember.send({ embeds: [embeddeny] }).catch((err) => {
+                    return interaction.reply(`
+                        This builder has DMs turned off or something went wrong while sending the DM.
+                    `)
+                });
 
                 await submissionMsg.react("❌");
+
+                const reviewerchanneldeny = new EmbedBuilder()
+                    .setDescription(`
+                    ${submittingMember.toString()}'s trial build was denied by ${reviewer}.\n[Submission Link](${submissionMsg.url}) 
+                    ${feedback != undefined ? `\nFeedback: ${feedback}` : ""}
+                    `)
+                    .setColor(0xFF0000)
 
                 DiscordHelpers.getTextChannel(client, client.config.opt.channels.hi_reviewers)
                     .then((channel: TextChannel | null) => {
                         if (channel && channel != null) {
-                            channel.send(`${submittingMember.tag}'s trial build was denied by ${reviewer}`);
+                            channel.send({ embeds: [reviewerchanneldeny] });
                         }
                     });
             }
